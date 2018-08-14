bb08000ae1261e795d5d76c9f065de711d545661
Rihards Veters <rihards.veters@dalder.com>
2018-08-14 14:00:56 +0300

    Convert to MVC design patern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rihards Veters on 14/08/2018.
+//  Copyright © 2018 Rihards Veters. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
