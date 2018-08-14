088efbd80c824415287d31b8e6b725927ac74ca2
Rihards Veters <rihards.veters@dalder.com>
2018-08-15 00:50:44 +0300

    Persisting data using Realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Rihards Veters on 14/08/2018.
+//  Copyright © 2018 Rihards Veters. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
